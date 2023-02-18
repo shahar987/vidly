17f7cb4605662b4eaa18b466c45475d74799f1b0
Tali148 <tali148@Talis-MBP.Dlink>
2023-02-18 17:37:23 +0200

    finish like feature

--- src/component/movies.jsx
+++ src/component/movies.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { getMovies } from "../services/fakeMovieService";
+import Like from "./common/like";
 
 function Movies(){
     const [state, setState] = useState({
@@ -7,16 +8,21 @@ function Movies(){
     })
     const [movieCount, setMovieCount] = useState(state.movies.length)
 
+
     const deleteMovie = (movie) =>{
         const movies = state.movies.filter(m => m._id !== movie._id)
         setState({movies: movies})
         setMovieCount(movieCount - 1)
     }
 
-    const test =()=>{
-        if (movieCount===0) return <p> there are no movies</p>
-        return <p>Showing {movieCount} in the datebase.</p>
-
+    const handleLike = (movie) =>{
+        console.log(state)
+        const movies2 = [...state.movies]
+        const index = movies2.indexOf(movie)
+        movies2[index] = {...movies2[index]}
+        movies2[index].liked = !movies2[index].liked
+        console.log(movies2)
+        setState({movies:movies2})
     }
     
     return(
@@ -33,15 +39,23 @@ function Movies(){
                         <th scope="col">Stock</th>
                         <th scope="col">Rate</th>
                         <th></th>
+                        <th></th>
                     </tr>
                 </thead>
                 <tbody>
                     {state.movies.map(movie =>(
+    
                         <tr key={movie._id}>
                             <td>{movie.title}</td>
                             <td>{movie.genre.name}</td>
                             <td>{movie.numberInStock}</td>
                             <td>{movie.dailyRentalRate}</td>
+                            <td>
+                                <Like 
+                                liked={movie.liked}
+                                onClick={()=>handleLike(movie)}
+                                />
+                            </td>
                             <td>
                                 <button type="button" className="btn btn-danger" onClick={() => deleteMovie(movie)}>Delete</button>
                             </td>
